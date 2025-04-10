2047120f0f291a21c2cbdfddd70493f3bffcbb4c
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2025-04-10 12:43:37 -0300

    Lista finalizada

---
+++ DOM.js
@@ -0,0 +1,12 @@
+export const DOM = {
+  containerTask: document.querySelector(".container-tasks"),
+  toggleList: document.querySelectorAll(".close-list-tasks"),
+
+  toggleTheme: document.querySelector(".btn-dark_mode"),
+
+  list: document.querySelector(".list-tasks"),
+  showTasks: document.querySelector(".show-tasks"),
+
+  addTask: document.querySelector(".add-task"),
+  addSubtask: document.querySelector(".add-subtask"),
+};
